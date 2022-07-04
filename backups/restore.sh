d4835fcd04e84963edeb4685d327c68209463da8
Timo Strackfeldt <timo@strackfeldt.com>
2022-07-04 14:31:45 +0200

    fix migrations

--- backups/restore.sh
+++ backups/restore.sh
@@ -6,4 +6,4 @@ fi
 
 # pg_restore -h infocenter-db-do-user-8430063-0.b.db.ondigitalocean.com -p 25060 -U infocenter -d infocenter --data-only latest.tar
 # pg_restore -U postgres -d infocenter --data-only latest.tar
-pg_restore -U postgres -d tracking --clean tracking.tar
+pg_restore -U postgres -d tracking --data-only local.tar
